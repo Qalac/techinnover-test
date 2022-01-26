3b2ac678c0048108a5528fb2677edf9710240cdd
Qalac <officialqalac@gmail.com>
2022-01-26 01:12:13 +0000

    initial commit

---
+++ index.py
@@ -0,0 +1,4 @@
+from rover import Rover
+
+first_rover = Rover(1,2,"N")
+print(first_rover.dir)
---
+++ rover.py
@@ -0,0 +1,14 @@
+class Rover:
+    def __init__(self, x_pos, y_pos, dir):
+        self.x_pos = x_pos
+        self.y_pos = y_pos
+        self.dir = dir
+
+    def moveLeft(self):
+        pass
+
+    def moveRight(self):
+        pass
+
+    def moveForward(self):
+        pass
